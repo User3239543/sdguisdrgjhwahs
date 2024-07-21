7d495053abb90deac307852f1be3702b193cf41f
User3239543 <devhazz18@gmail.com>
2024-07-21 05:09:56 +0100

    Create trbuirtuj.lua

---
+++ trbuirtuj.lua
@@ -0,0 +1,31 @@
+local Console =  loadstring(game:HttpGet("https://raw.githubusercontent.com/CasparLmao/Ui-Libs/Main/Stx%20Console/stx.lua"))()
+local ConsoleLog = Console:Window({
+    Title = "[RIP SYSBLOCK] Bypass Anti Cheat | 21/07/24",
+    Position = UDim2.new(0.5, 0, 0.5, 0),
+    DragSpeed = 12
+})
+ConsoleLog:Prompt({
+    Title = "Bypassing Anti Cheat...",
+    TypesWeHave = {
+        "default",
+        "success",
+        "fail",
+        "warning",
+        "nofitication"
+    },
+    Type = "warning"
+})
+wait(12)
+ConsoleLog:Prompt({
+    Title = "Completed",
+    TypesWeHave = {
+        "default",
+        "success",
+        "fail",
+        "warning",
+        "nofitication"
+    },
+    Type = "success"
+    
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Misc-Script/main/adonis%20ac%20bypass.lua"))()
